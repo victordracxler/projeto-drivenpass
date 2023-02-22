e8c28f4a151a02ad92b51c29339f0407f113a061
Victor Dracxler <victordracxler@poli.ufrj.br>
2023-02-22 12:30:53 -0300

    feat: prisma init

--- src/app.ts
+++ src/app.ts
@@ -8,39 +8,21 @@ import { loadEnv, connectDb, disconnectDB } from '@/config';
 loadEnv();
 
 import { handleApplicationErrors } from '@/middlewares';
-import {
-  usersRouter,
-  authenticationRouter,
-  eventsRouter,
-  enrollmentsRouter,
-  ticketsRouter,
-  paymentsRouter,
-  hotelsRouter,
-  bookingsRouter,
-} from '@/routers';
+import {} from '@/routers';
 
 const app = express();
-app
-  .use(cors())
-  .use(express.json())
-  .get('/health', (_req, res) => res.send('OK!'))
-  .use('/users', usersRouter)
-  .use('/auth', authenticationRouter)
-  .use('/event', eventsRouter)
-  .use('/enrollments', enrollmentsRouter)
-  .use('/tickets', ticketsRouter)
-  .use('/payments', paymentsRouter)
-  .use('/hotels', hotelsRouter)
-  .use('/booking', bookingsRouter)
-  .use(handleApplicationErrors);
+app.use(cors())
+	.use(express.json())
+	.get('/health', (_req, res) => res.send('OK!'))
+	.use(handleApplicationErrors);
 
 export function init(): Promise<Express> {
-  connectDb();
-  return Promise.resolve(app);
+	connectDb();
+	return Promise.resolve(app);
 }
 
 export async function close(): Promise<void> {
-  await disconnectDB();
+	await disconnectDB();
 }
 
 export default app;
